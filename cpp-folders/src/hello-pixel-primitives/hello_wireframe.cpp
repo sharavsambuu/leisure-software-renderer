10b4d8cf702765e7a53839ecf591723bcc0e3316
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2023-08-23 04:42:39 +0800

    first 3d obj rendering using lines.

--- cpp-folders/src/hello-pixel-primitives/hello_wireframe.cpp
+++ cpp-folders/src/hello-pixel-primitives/hello_wireframe.cpp
@@ -14,6 +14,10 @@ int main()
 
     std::cout << "Hello Wireframe" << std::endl;
 
+    int canvas_width  = 600;
+    int canvas_height = 600;
+    shs::Canvas canvas = shs::Canvas(canvas_width, canvas_height, shs::Color::black());
+
     Assimp::Importer importer;
     const aiScene *scene = importer.ReadFile("./obj/monkey/monkey.rawobj", aiProcess_Triangulate);
     if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
@@ -30,28 +34,34 @@ int main()
         for (unsigned int j = 0; j < mesh->mNumFaces; j++)
         {
             aiFace face = mesh->mFaces[j];
-
             for (unsigned int k = 0; k < face.mNumIndices; k++)
             {
                 unsigned int vertex_index = face.mIndices[k];
                 aiVector3D vertex = mesh->mVertices[vertex_index];
-
                 if (k > 0)
                 {
-                    float x0 = prev_vertex.x;
-                    float y0 = prev_vertex.y;
-                    float x1 = vertex.x;
-                    float y1 = vertex.y;
-
-                    std::cout << "Line: (" << x0 << ", " << y0 << "), (" << x1 << ", " << y1 << ")" << std::endl;
+                    int x0 = (prev_vertex.x + 1.0) * canvas_width / 2.0;
+                    int y0 = (prev_vertex.y + 1.0) * canvas_height / 2.0;
+                    int x1 = (vertex.x + 1.0) * canvas_width / 2.0;
+                    int y1 = (vertex.y + 1.0) * canvas_height / 2.0;
+                    if (
+                        (x0 > 0 && x0 < canvas_width) &&
+                        (y0 > 0 && y0 < canvas_height) &&
+                        (x1 > 0 && x1 < canvas_width) &&
+                        (y1 > 0 && y1 < canvas_height))
+                    {
+                        //std::cout << x0 << " " << y0 << " " << x1 << " " << y1 << std::endl;
+                        shs::Canvas::draw_line(canvas, x0, y0, x1, y1, shs::Color::green());
+                    }
                 }
-
                 prev_vertex = vertex;
             }
         }
     }
+    shs::Canvas::flip_horizontally(canvas); // origin at the left bottom corner of the canvas
+    canvas.save_png("hello_wireframe_monkeyobj_canvas.png" );
 
-    std::cout << "end." << std::endl;
+    std::cout << "done." << std::endl;
 
     return EXIT_SUCCESS;
 }
