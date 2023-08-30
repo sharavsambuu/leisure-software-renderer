dc06e9c2c2877e51074e5235314516584e611046
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2023-08-31 07:26:02 +0800

    Fixed segmentation fault issue and actually rendering with SHS API in realtime.

--- cpp-folders/src/hello-pixel-primitives/hello_sdl_pixel.cpp
+++ cpp-folders/src/hello-pixel-primitives/hello_sdl_pixel.cpp
@@ -3,6 +3,7 @@
 
 #define WINDOW_WIDTH  640
 #define WINDOW_HEIGHT 480
+
 #define CANVAS_WIDTH  256
 #define CANVAS_HEIGHT 256
 
@@ -20,19 +21,26 @@ int main()
 
     shs::Canvas *main_canvas     = new shs::Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
     SDL_Surface *main_sdlsurface = main_canvas->create_sdl_surface();
-    SDL_Texture* screen_texture  = SDL_CreateTextureFromSurface(renderer, main_sdlsurface);
+    SDL_Texture *screen_texture  = SDL_CreateTextureFromSurface(renderer, main_sdlsurface);
 
 
     bool exit = false;
-    SDL_Event eventData;
+    SDL_Event event_data;
     while (!exit)
     {
-        while (SDL_PollEvent(&eventData))
+        while (SDL_PollEvent(&event_data))
         {
-            switch (eventData.type)
+            switch (event_data.type)
             {
             case SDL_QUIT:
                 exit = true;
+                break;
+            case SDL_KEYDOWN:
+                switch(event_data.key.keysym.sym) {
+                    case SDLK_ESCAPE: 
+                        exit = true;
+                        break;
+                }
                 break;
             }
         }
@@ -42,13 +50,13 @@ int main()
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // SHS rendering
-        //shs::Canvas::fill_pixel(*main_canvas, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, shs::Pixel::blue_pixel());
+        // Software rendering or drawing stuffs goes around here
+        shs::Canvas::fill_pixel(*main_canvas, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT, shs::Pixel::blue_pixel());
         shs::Canvas::fill_pixel(*main_canvas, 10, 10, 20, 30, shs::Pixel::white_pixel());
         shs::Canvas::fill_random_pixel(*main_canvas, 40, 30, 60, 80);
-        //shs::Canvas::flip_horizontally(*main_canvas); // origin at the left bottom corner of the canvas
 
-        // actually prensenting on hardware surface
+        // actually prensenting canvas data on hardware surface
+        shs::Canvas::flip_horizontally(*main_canvas); // origin at the left bottom corner of the canvas
         shs::Canvas::copy_to_SDLSurface(main_sdlsurface, main_canvas);
         SDL_UpdateTexture(screen_texture, NULL, main_sdlsurface->pixels, main_sdlsurface->pitch);
         SDL_Rect destination_rect{0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
