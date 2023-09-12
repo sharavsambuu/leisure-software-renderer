33e56e4f83dda94adb1e48008cf5a518490f2475
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2023-09-12 23:30:57 +0800

    spawining worker threads

--- cpp-folders/src/hello-parallelization/hello_fiber_job_system.cpp
+++ cpp-folders/src/hello-parallelization/hello_fiber_job_system.cpp
@@ -7,25 +7,58 @@
 
 #define CONCURRENCY_COUNT 4
 
+class JobSystem
+{
+public:
+    JobSystem(int concurrency_count) 
+    {
+        std::cout << "Job system is starting..." << std::endl;
+
+        this->concurrency_count = concurrency_count;
+        this->workers.reserve(this->concurrency_count);
+
+        for (int i = 0; i < this->concurrency_count; ++i)
+        {
+            this->workers[i] = boost::thread([this, i] {
+                boost::fibers::use_scheduling_algorithm<boost::fibers::algo::work_stealing>(this->concurrency_count);
+                std::cout << "thread " << i << " is on duty." << std::endl;
+
+            });
+        }
+    }
+    ~JobSystem()
+    {
+        for (auto &worker : this->workers)
+        {
+            worker.join();
+        }
+        std::cout << "Job system is shutting down..." << std::endl;
+    }
+private:
+
+    int concurrency_count;
+
+    std::vector<boost::thread> workers;
+};
 
 int main()
 {
 
+    JobSystem *job_system = new JobSystem(CONCURRENCY_COUNT);
+
     bool is_engine_running = true;
-
-    auto stop_time = std::chrono::steady_clock::now() + std::chrono::seconds(10);
-
+    auto stop_time = std::chrono::steady_clock::now() + std::chrono::seconds(5);
     while (is_engine_running)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-        std::cout << "engine is running" << std::endl;
 
         if (std::chrono::steady_clock::now() > stop_time)
         {
             is_engine_running = false;
         }
     }
+
+    delete job_system;
 
     std::cout << "system is shutting down... bye!" << std::endl;
 
