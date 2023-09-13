31069daf633a143356f68c85d6e236d74de51bb3
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2023-09-13 21:24:47 +0800

    added task manager function for coordinating group of workers

--- cpp-folders/src/hello-parallelization/hello_fiber_job_system_graph.cpp
+++ cpp-folders/src/hello-parallelization/hello_fiber_job_system_graph.cpp
@@ -10,28 +10,31 @@
  *  A -> & -> D -> E
  *       C
  * 
- * Meaning B and C groups of should work in concurrent manner and other group
- * should follow this graph
+ * Meaning B and C groups should work in concurrent manner and other group
+ * should follow this graph, kind of counting on work stealing mode on those
+ * group of workers in order to juicing many CPU cores.
  * 
-*/
+ * 
+ */
+
 
 #define CONCURRENCY_COUNT 4
 
-void send_batch_jobs(shs::AbstractJobSystem &job_system, int priority)
+void run_task_manager(shs::AbstractJobSystem &job_system)
 {
     for (int i = 0; i < 2000; ++i)
     {
         job_system.submit({[i] {
-            std::cout << "Job " << i << " started" << std::endl;
+            //std::cout << "Job " << i << " started" << std::endl;
             for (int j=0; j<200; ++j)
             {
-                std::cout << "Job " << i << " is working..." << std::endl;
+                //std::cout << "Job " << i << " is working..." << std::endl;
                 boost::this_fiber::yield(); // let's be nice with each other
             }
             boost::this_fiber::yield();
-            std::cout << "Job " << i << " finished" << std::endl; 
+            //std::cout << "Job " << i << " finished" << std::endl; 
         },
-        priority
+        shs::JobPriority::NORMAL
         });
     }
 }
@@ -41,25 +44,21 @@ int main()
 
     shs::AbstractJobSystem *lockless_job_system = new shs::LocklessPriorityJobSystem(CONCURRENCY_COUNT);
 
-    bool is_engine_running = true;
-
-    auto first_stop_time = std::chrono::steady_clock::now() + std::chrono::seconds(5);
-    bool is_sent_second_batch = false;
-    auto second_stop_time = std::chrono::steady_clock::now() + std::chrono::seconds(30);
-
-    std::cout << ">>>>> sending first batch jobs" << std::endl;
-    send_batch_jobs(*lockless_job_system, shs::JobPriority::NORMAL);
+    bool is_engine_running    = true;
+    auto first_stop_time      = std::chrono::steady_clock::now() + std::chrono::seconds(5);
+    bool did_run_task_manager = false;
+    auto second_stop_time     = std::chrono::steady_clock::now() + std::chrono::seconds(60);
 
     while (is_engine_running)
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
 
-        if (std::chrono::steady_clock::now() > first_stop_time && !is_sent_second_batch)
+        if (std::chrono::steady_clock::now() > first_stop_time && !did_run_task_manager)
         {
 
-            std::cout << ">>>>> sending second batch jobs to the lockless priority workers" << std::endl;
-            send_batch_jobs(*lockless_job_system, shs::JobPriority::HIGH);
-            is_sent_second_batch = true;
+            std::cout << "STATUS : Starting a task manager... " << std::endl;
+            run_task_manager(*lockless_job_system);
+            did_run_task_manager = true;
         }
 
         if (std::chrono::steady_clock::now() > second_stop_time)
@@ -67,11 +66,13 @@ int main()
             is_engine_running = false;
             lockless_job_system->is_running = false;
         }
+
+        std::cout << "STATUS : Main thread is running..." << std::endl;
     }
 
     delete lockless_job_system;
 
-    std::cout << "system is shutting down... bye!" << std::endl;
+    std::cout << "STATUS : All system is shutting down... BYE!" << std::endl;
 
     return 0;
 }
